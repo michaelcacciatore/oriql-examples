66ff6bcb6a8c003aaee0d290fe33674eb038db21
Michael Cacciatore <mcacciatore116@gmail.com>
2019-09-24 20:31:26 -0400

    update TS example

--- with-typescript/src/components/Hello.tsx
+++ with-typescript/src/components/Hello.tsx
@@ -1,13 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
 import {PropTypes} from '../../types';
 
-const displayName = 'myComponent';
-
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export default class MyComponent extends React.Component<PropTypes.Hello, {}> {
-  static displayName = displayName;
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.foo.foo}!</h1>;
-    }
-}
+export default ({ compiler, framework }: PropTypes.Hello) => (
+  <h1>Hello from {compiler} and {framework}!</h1>
+);
